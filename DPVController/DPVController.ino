f560560079bd6cee23d26ae6b5fba39be71944ba
Carlos Freund <carlosfreund@googlemail.com>
2023-04-16 12:43:32 +0200

    introduced speedSetting variable

--- DPVController/DPVController.ino
+++ DPVController/DPVController.ino
@@ -1,42 +1,41 @@
 #include <Servo.h>
 #include <PinButton.h>
 
-
+// PIN constants
 const int PIN_LEFT_BUTTON = 4; //D2
 const int PIN_RIGHT_BUTTON = 5; //D1
-
 const int PIN_LEAK = 16; //D0
-
 const int PIN_MOTOR = 0; //D3
 
+// Values for motorState
 const int MOTOR_OFF = 0;
 const int MOTOR_ON = 1;
 const int MOTOR_STANDBY = 2;
 
+//Constants
 const int MOTOR_MAX_SPEED = 160;
 const int MOTOR_MIN_SPEED = 5;
-const int SPEED_UP_TIME_MS = 4000;
+const int SPEED_UP_TIME_MS = 4000; //time we want to take to  speed the motor from 0 to  full power.
+const int SPEED_STEPS = 20; //Number speed steps
+const int MOTOR_SPEED_CHANGE = MOTOR_MAX_SPEED/SPEED_STEPS;
 
-const int SPEED_STEPS = 0.05;
-const int MOTOR_SPEED_CHANGE = MOTOR_MAX_SPEED*0.05;
 
-
+//Variables
 int leftButtonState = 0;
 int rightButtonState = 0;
 int leakSensorState = 0;
 int motorState = MOTOR_OFF;
 int currentMotorSpeed = 0; //Speed the motor is currently running at
-int targetMotorSpeed = MOTOR_MIN_SPEED;
-int motorStartTime = 0;
+int currentMotorTime = 0; //Time in MS when we last changed the currentMotorSpeed
+int speedSetting = MOTOR_MIN_SPEED; //The current speed setting. stays the same, even if motor is turned off. 
+int targetMotorSpeed = 0; //The desired motor speed
 
+
+//IO
 PinButton leftButton(PIN_LEFT_BUTTON);
 PinButton rightButton(PIN_RIGHT_BUTTON);
 PinButton LeakSensor(PIN_LEAK);
-
-
 Servo servo;
-
-
 
 void setup() {
   pinMode(PIN_LEFT_BUTTON, INPUT);
@@ -51,32 +50,28 @@ void setup() {
 
 }
 
-void updateTargetMotorSpeed(){
+void updateSpeedSetting(){
   if (rightButton.isDoubleClick()){
-    targetMotorSpeed += MOTOR_SPEED_CHANGE;
-    if (targetMotorSpeed > MOTOR_MAX_SPEED){
-      targetMotorSpeed = MOTOR_MAX_SPEED;
+    speedSetting += MOTOR_SPEED_CHANGE;
+    if (speedSetting > MOTOR_MAX_SPEED){
+      speedSetting = MOTOR_MAX_SPEED;
     }
   }
 
   if (leftButton.isDoubleClick()){
-    targetMotorSpeed -= MOTOR_SPEED_CHANGE;
-    if (targetMotorSpeed < MOTOR_MIN_SPEED){
-      targetMotorSpeed = MOTOR_MIN_SPEED;
+    speedSetting -= MOTOR_SPEED_CHANGE;
+    if (speedSetting < MOTOR_MIN_SPEED){
+      speedSetting = MOTOR_MIN_SPEED;
     }
   }
 
-  Serial.print(" targetMotorSpeed: ");
-  Serial.print(targetMotorSpeed);
+  Serial.print(" speedSetting: ");
+  Serial.print(speedSetting);
 
 }
 
 void controlMotor(){
   if((leftButtonState == 1 || rightButtonState == 1) && leakSensorState == 0){
-    //motor was just turned on
-    if (motorState != MOTOR_ON){
-      motorStartTime = millis();
-    }
     motorState = MOTOR_ON;
   }else{
     motorState = MOTOR_OFF;
@@ -86,24 +81,18 @@ void controlMotor(){
 
   if (motorState == MOTOR_STANDBY || motorState == MOTOR_OFF){
     //Motor is off
-    servo.write(0);
-    currentMotorSpeed = 0;
     targetMotorSpeed = 0;
   }else if (motorState == MOTOR_ON){
 
-    int diff = millis() - motorStartTime;
-    int end_speed_up = SPEED_UP_TIME_MS * targetMotorSpeed / MOTOR_MAX_SPEED;
-    if (diff >= end_speed_up){
-      currentMotorSpeed = targetMotorSpeed; 
-    }else{
-      currentMotorSpeed = targetMotorSpeed * diff / end_speed_up;
-    }
-    servo.write(currentMotorSpeed);
   }
   Serial.print(" currentMotorSpeed: ");
   Serial.print(currentMotorSpeed);
 }
 
+
+void setSoftMotorSpeed(){
+
+}
 
 
 void loop() {
@@ -126,7 +115,7 @@ void loop() {
   Serial.print(" millis: ");
   Serial.print(millis());
 
-  updateTargetMotorSpeed();
+  updateSpeedSetting();
   controlMotor();
 
   Serial.println();
