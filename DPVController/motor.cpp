d8b13c3b1a01b5483105ce3a2af52f9de42159d5
n3ro <christoph.bubeck@chbu.org>
2023-12-30 17:57:43 +0100

    prevent entering standup after leaving cruise control

--- DPVController/motor.cpp
+++ DPVController/motor.cpp
@@ -220,6 +220,7 @@ void enterCruiseMode(){
 void leaveCruiseMode(){
   log("leaving cruise mode", 0);
   motorState = off;
+  lastActionTime = micros();//Prevent standby right after leaving cruise control
 }
 
 void enterTurboMode(){
