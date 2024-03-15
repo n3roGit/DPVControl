277994c846a5743c5fd44dae2c94ca7bf6bb05e4
Carlos Freund <carlosfreund@googlemail.com>
2024-03-15 21:25:25 +0100

    only trigger sensor leak after 1s.

--- DPVController/leak.cpp
+++ DPVController/leak.cpp
@@ -6,24 +6,31 @@
 
 int leakSensorState = 0;
 
+int lastNoLeakMs = 0;
 
-void leakSetup(){
+void leakSetup()
+{
   pinMode(PIN_LEAK_FRONT, INPUT_PULLUP);
   pinMode(PIN_LEAK_BACK, INPUT_PULLUP);
 }
 
-bool getLeakFront(){
-    return digitalRead(PIN_LEAK_FRONT) == LOW;
+bool getLeakFront()
+{
+  return digitalRead(PIN_LEAK_FRONT) == LOW;
 }
-bool getLeakBack(){
-    return digitalRead(PIN_LEAK_BACK) == LOW;
+bool getLeakBack()
+{
+  return digitalRead(PIN_LEAK_BACK) == LOW;
 }
 
-void checkForLeak() {
+void checkForLeak()
+{
 
-  if (getLeakFront() || getLeakBack()) {
-    leakSensorState = 1;  // There is a leak
+  if ((getLeakFront() || getLeakBack()) && millis() - lastNoLeakMs >= 1000){
+    leakSensorState = 1; // There is a leak
     log("leakSensorState", leakSensorState, true);
-    setBarLeak();    
+    setBarLeak();
+  }else{
+    lastNoLeakMs = millis();
   }
 }
