a1e642967beaf59a15cb2eab5ad2ce3bdf3a779c
n3ro <christoph.bubeck@chbu.org>
2025-05-25 11:29:36 +0200

    added lib version

--- DPVController/DPVController.ino
+++ DPVController/DPVController.ino
@@ -1,15 +1,15 @@
 /*
 *   LIBRARIES
 */
-#include "ClickButton.h"  //https://github.com/marcobrianza/ClickButton
-#include "DHTesp.h"  //https://github.com/beegee-tokyo/DHTesp
+#include "ClickButton.h"  //https://github.com/marcobrianza/ClickButton (v1.1.0)
+#include "DHTesp.h"  //https://github.com/beegee-tokyo/DHTesp (v1.19)
 #include <HardwareSerial.h>
-#include <VescUart.h>  //https://github.com/SolidGeek/VescUart
-#include "uptime_formatter.h"  //https://github.com/YiannisBourkelis/Uptime-Library
-#include <Adafruit_NeoPixel.h>  //https://github.com/adafruit/Adafruit_NeoPixel
+#include <VescUart.h>  //https://github.com/SolidGeek/VescUart (v1.0.1)
+#include "uptime_formatter.h"  //https://github.com/YiannisBourkelis/Uptime-Library (v1.0.0)
+#include <Adafruit_NeoPixel.h>  //https://github.com/adafruit/Adafruit_NeoPixel (v1.12.0)
 
-#include "FS.h" //Provided by framework
-#include "SPIFFS.h"//Provided by framework
+#include "FS.h" //Provided by framework (v3.2.0)
+#include "SPIFFS.h"//Provided by framework (v3.2.0)
 
 
 #include "main.h"
